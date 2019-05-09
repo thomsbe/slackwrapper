02c63cb0e5487de3ca647a0d98d44b943a1f72c3
Thomas Baer <thomas.baer@tbaer.eu>
2019-05-09 13:52:07 +0200

    Added more setup.py stuff

--- setup.py
+++ setup.py
@@ -4,15 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="slackwrapper-thomsbe",
-    version="0.0.2",
+    name="slackwrapper",
+    version="1.0.0.dev1",
     author="Thomas Baer",
     author_email="thomas.baer@tbaer.eu",
     description="A small wrapper to easily use the slack client",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/thomsbe/slackwrapper",
-    packages="slackclient",
+    packages=setuptools.find_packages(exclude=['contrib', 'docs', 'tests*']),
+    python_requires='>=3',
+    install_requires=['slackclient'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
